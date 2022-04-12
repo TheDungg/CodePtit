cba3d4d260b56aeb0ee4164110039046867adb4b
TheDungg <88679196+TheDungg@users.noreply.github.com>
2022-04-12 21:31:52 +0700

    Add files via upload

--- PhanTichThuaSoNguyenTo.cpp
+++ PhanTichThuaSoNguyenTo.cpp
@@ -1,20 +1,26 @@
 #include<stdio.h>
-int main(){
-	int n;
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++){
-		printf("\n");
-		int a;
-		scanf("%d",&a);
-		int j=2;
-		while(a>1){
-			if(a%j==0){
-				printf("%2d",j);
-				a/=j;
-			}else{
-				j++;
-			}
+void pt (long long n) {
+	printf("%lld = ", n);
+	for (int i=2; i<=n; i++) {
+		int count = 0;
+		while (n%i == 0) {
+			count++;
+			n /= i;
 		}
+		if(count != 0){
+			printf("%d^%d", i, count);
+			if(n>i)	printf(" * ");
+		}	
+	}
+}
+int main () {
+	int t;
+	scanf ("%d", &t);
+	while (t--) {
+		long long n;
+		scanf ("%lld", &n);
+		pt(n);
+		printf("\n");
 	}
 	return 0;
 }
